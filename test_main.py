b7e5c0887b7151977b2d6dfbcc3da2a14a19ddef
Zainab <zainabhomsi11@gmail.com>
2026-03-17 06:59:27 +0300

    test2

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add_numbers, divide_numbers
 
 class TestMain(unittest.TestCase):
     def test_add_numbers(self):
-        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(2, 3), 6)
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertAlmostEqual(add_numbers(0.1, 0.2), 0.3)
         self.assertAlmostEqual(add_numbers(-1, -2), -3)
